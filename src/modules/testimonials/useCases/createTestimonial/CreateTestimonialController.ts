6b9cbfe31344270a8b7500c1f3df18596e226926
Welisson <welissonluca17@gmail.com>
2021-07-24 20:12:38 -0300

    feat: create testimonial controller

---
+++ src/modules/testimonials/useCases/createTestimonial/CreateTestimonialController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateTestimonialCompanyUseCase } from './CreateTestimonialUseCase';
+
+interface ITestimonialRequest {
+  name: string;
+  avatar_url: string;
+  city: string;
+  office: string;
+  relation: string;
+}
+
+class CreateTestimonialController {
+  async handle(request: Request, response: Response) {
+    const { name, avatar_url, city, office, relation }: ITestimonialRequest =
+      request.body;
+
+    const createTestimonialCompanyUseCase = container.resolve(
+      CreateTestimonialCompanyUseCase
+    );
+    const newCompany = await createTestimonialCompanyUseCase.execute({
+      name,
+      avatar_url,
+      city,
+      office,
+      relation,
+    });
+
+    return response.status(201).json(newCompany);
+  }
+}
+
+export { CreateTestimonialController };
