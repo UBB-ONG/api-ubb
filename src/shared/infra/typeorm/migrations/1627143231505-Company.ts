5aa56fc4bae8d827bd3875c2df4278da289f1c1b
Welisson <welissonluca17@gmail.com>
2021-07-24 13:18:44 -0300

    feat: add migration create tesmonial and companies

---
+++ src/shared/infra/typeorm/migrations/1627143231505-Company.ts
@@ -0,0 +1,38 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class Company1627143231505 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+					new Table({
+						name: "companies",
+						columns: [
+							{
+								name: "id",
+								type: "int",
+								isPrimary: true,
+							},
+							{
+								name: "avatar_url",
+								type: "varchar",
+							},
+							{
+								name: "created_at",
+								type: "timestamp",
+								default: "now()",
+							},
+							{
+								name: "updated_at",
+								type: "timestamp",
+								default: "now()",
+							},
+						],
+					})
+				);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("companies");
+    }
+
+}
