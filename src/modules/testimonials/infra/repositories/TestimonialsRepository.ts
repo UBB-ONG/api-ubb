a164f4cb5d8dd7b265e017204acbad4316c3ef8d
Welisson <welissonluca17@gmail.com>
2021-07-24 19:55:58 -0300

    feat: add testiminials repository

---
+++ src/modules/testimonials/infra/repositories/TestimonialsRepository.ts
@@ -0,0 +1,51 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { ICreateTestimonialDTO } from '@modules/testimonials/dtos/ICreateTestimonialDTO';
+import { ITestimonialRepository } from '@modules/testimonials/repositories/ITestimonialRepository';
+
+import { Testimonial } from '../entities/Testimonial';
+
+class TestimonialsRepository implements ITestimonialRepository {
+  private repository: Repository<Testimonial>;
+
+  constructor() {
+    this.repository = getRepository(Testimonial);
+  }
+
+  async create({
+    name,
+    avatar_url,
+    city,
+    office,
+    relation,
+  }: ICreateTestimonialDTO): Promise<Testimonial> {
+    const newTestimonial = this.repository.create({
+      name,
+      avatar_url,
+      city,
+      office,
+      relation,
+    });
+
+    const result = await this.repository.save(newTestimonial);
+
+    return result;
+  }
+
+  async findById(id: string): Promise<Testimonial> {
+    const testimonial = await this.repository.findOne({ id });
+    return testimonial;
+  }
+
+  async list(): Promise<Testimonial[]> {
+    const all = await this.repository.find();
+    return all;
+  }
+
+  async delete(id: string): Promise<void> {
+    const testimonial = await this.repository.findOne({ id });
+    await this.repository.remove(testimonial);
+  }
+}
+
+export { TestimonialsRepository };
