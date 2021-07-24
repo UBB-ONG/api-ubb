7b9e060e73452cb54db68d1ab785e47ecb63e37e
Welisson <welissonluca17@gmail.com>
2021-07-24 20:12:29 -0300

    feat: add create testimonial use case

---
+++ src/modules/testimonials/useCases/createTestimonial/CreateTestimonialUseCase.ts
@@ -0,0 +1,46 @@
+import { inject, injectable } from 'tsyringe';
+
+import { AppError } from '@shared/errors/AppError';
+import { Validator } from '@shared/utils/Validators';
+
+import { Testimonial } from '../../infra/typeorm/entities/Testimonial';
+import { ITestimonialRepository } from '../../repositories/ITestimonialRepository';
+
+interface ITestimonialRequest {
+  name: string;
+  avatar_url: string;
+  city: string;
+  office: string;
+  relation: string;
+}
+const validator = new Validator();
+@injectable()
+class CreateTestimonialCompanyUseCase {
+  constructor(
+    @inject('TestimonialRepository')
+    private testimonialRepository: ITestimonialRepository
+  ) {}
+  async execute({
+    name,
+    avatar_url,
+    city,
+    office,
+    relation,
+  }: ITestimonialRequest): Promise<Testimonial> {
+    if (!avatar_url) throw new AppError('Photo incorrect!');
+    if (!name) throw new AppError('Name incorrect!');
+    await validator.validName(name);
+    await validator.validUrl(avatar_url);
+    const result = this.testimonialRepository.create({
+      name,
+      avatar_url,
+      city,
+      office,
+      relation,
+    });
+
+    return result;
+  }
+}
+
+export { CreateTestimonialCompanyUseCase };
