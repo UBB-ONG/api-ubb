9442ec5078f48e446324ffe5f344f157b103152a
Welisson <welissonluca17@gmail.com>
2021-07-25 00:57:53 -0300

    feat: add routes list and delete testimonial

--- src/shared/infra/http/routes/testimonial.routes.ts
+++ src/shared/infra/http/routes/testimonial.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express';
 
 import { CreateTestimonialController } from '@modules/testimonials/useCases/createTestimonial/CreateTestimonialController';
+import { DeleteTestimonialController } from '@modules/testimonials/useCases/deleteTestimonial/DeleteTestimonialController';
+import { ListTestimonialController } from '@modules/testimonials/useCases/listTestimonials/ListTestimonialController';
 
 const testimonialRouter = Router();
 
 const createTestimonialController = new CreateTestimonialController();
-
+const listTestimonialController = new ListTestimonialController();
+const deleteTestimonialController = new DeleteTestimonialController();
 testimonialRouter.post('/testimonial', createTestimonialController.handle);
+testimonialRouter.get('/testimonial', listTestimonialController.handle);
+testimonialRouter.delete(
+  '/testimonial/:id',
+  deleteTestimonialController.handle
+);
 
 export { testimonialRouter };
