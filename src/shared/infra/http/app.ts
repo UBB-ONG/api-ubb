c779c820a0ff6d5bd20451f1c6560a9d185acf5d
Welisson <welissonluca17@gmail.com>
2021-07-25 12:39:43 -0300

    fix: swagger documentation conflicts

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -14,8 +14,9 @@ import { router } from './routes';
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded());
 app.use(cors());
-
+  
 app.use(router);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
